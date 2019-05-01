591fa27ace1f4923f64a0585f4fb8101a72fadee
jvdprates <49961428+jvdprates@users.noreply.github.com>
2019-05-01 11:38:24 -0300

    Add files via upload
    Declaração do tipo Índice, implementação de NoLeitor e declaração do tipo Leitor.

--- Leitor de Arquivos/leitor.h
+++ Leitor de Arquivos/leitor.h
@@ -8,7 +8,50 @@ e ignorando Maiusculas e Minusculas.  */
 #ifndef LEITOR_H
 #define LEITOR_H
 
+#include <string>
 
+struct NoLeitor;
+
+class Indice {
+    public:
+        // Cria um indice invertido vazio.
+        Indice();
+
+        // Cria um indice com os mesmos elementos de x.
+        Indice(const Indice& x);
+
+        // Testa se um indice esta vazio.
+        bool vazio() const;
+
+        // Retorna o numero de elementos de um indice.
+        int tamanho() const;
+
+        // Retorna o primeiro elemento de um indice.
+        // PRECONDICAO: o indice nao pode estar vazio.
+        string primeiro() const;
+
+        // Compara de *this tem os mesmos elementos de x.
+        bool operator==(const Indice& x) const;
+
+        // Faz com que *this tenha os mesmos elementos de x.
+        void operator=(const Indice& x);
+
+        // Desaloca a memoria de um indice.
+        ~Indice();
+
+    private:
+        // Ponteiro do sentinela de um indice.
+        NoLeitor* fim_;
+
+        // Numero de elementos de um indice.
+        int tam_;
+};
+
+class Leitor {
+    public:
+
+    private:
+};
 
 
 #endif // LEITOR_H
