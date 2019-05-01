c77b16b4780b646a6c3b277f7c3e9f439a3a46a7
jvdprates <49961428+jvdprates@users.noreply.github.com>
2019-05-01 11:06:31 -0300

    Add files via upload

---
+++ Leitor de Arquivos/leitor.cpp
@@ -0,0 +1,3 @@
+/* Aqui e onde sera realizada a implementacao das funcoes do arquivo leitor.h */
+
+#include "leitor.h"
---
+++ Leitor de Arquivos/leitor.h
@@ -0,0 +1,14 @@
+/* Aqui vao ser declarados todas as funcoes relacionadas ao leitor de arquivos da primeira tarefa.
+
+O objetivo e criar um Leitor que consegue abrir todos os arquivos .txt de um diretorio, armazenar
+em forma de strings todas as palavras destes arquivos, assim como o numero de vezes em que elas aparecem
+e por fim gerar um Indice Invertido contendo cada um dos valores armazenados, retirando sinais de pontuacao
+e ignorando Maiusculas e Minusculas.  */ 
+
+#ifndef LEITOR_H
+#define LEITOR_H
+
+
+
+
+#endif // LEITOR_H
