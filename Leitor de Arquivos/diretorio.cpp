7e19981fc25a2283dd77e5761be3bdaa2d6c2516
João Vítor David Prates <49961428+jvdprates@users.noreply.github.com>
2019-06-11 11:14:29 -0300

    Pequenas alterações de legibilidade em V4.0
    Reescrevi os comentários e os nomes de variáveis para melhorar a legibilidade destes dois arquivos.

--- Leitor de Arquivos/diretorio.cpp
+++ Leitor de Arquivos/diretorio.cpp
@@ -1,14 +1,14 @@
 #include "diretorio.h"
 
-Diretorio::Diretorio(){
+Diretorio::Diretorio(){ //O construtor padrao de um Diretorio por default aponta para o Sumario.
     prefixo_ = "Diretorio\\\\";
     nomeArquivo_ = "Sumario.txt";
     concatenarEndereco();
 }
 
-Diretorio::Diretorio(string E, string N){
-    prefixo_ = E;
-    nomeArquivo_ = N;
+Diretorio::Diretorio(string Endereco, string Nome){
+    prefixo_ = Endereco;
+    nomeArquivo_ = Nome;
     concatenarEndereco();
 }
 
@@ -24,12 +24,12 @@ string Diretorio::lerNomeCompleto() const{
     return nomeCompleto_;
 }
 
-void Diretorio::setNome(string S){
-    nomeArquivo_ = S;
+void Diretorio::setNome(string Nome){
+    nomeArquivo_ = Nome;
 }
 
-void Diretorio::setEndereco(string S){
-    prefixo_ = S;
+void Diretorio::setEndereco(string Endereco){
+    prefixo_ = Endereco;
 }
 
 void Diretorio::concatenarEndereco(){
