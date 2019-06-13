ae61d59558e35da19dfbc4873c28e9be2f72bfc4
João Vítor David Prates <49961428+jvdprates@users.noreply.github.com>
2019-06-13 02:26:57 -0300

    Modelo de main.cpp final
    Esta versão serve como base para finalização do nosso trabalho.

--- Leitor de Arquivos/main.cpp
+++ Leitor de Arquivos/main.cpp
@@ -1,71 +1,260 @@
-#include "indice.h"
-#include "busca.h"
-#include <fstream>
+// Copyright 2010 Universidade Federal de Minas Gerais (UFMG)
+//
+// Lista de exercícios sobre Tipos Abstratos de Dados.
+// Aplicação: Números Complexos - Coordenadas Euclidianas.
+//
+// QUESTÃO 1:
+// Implemente em complexo.cpp as funções descritas em complexo.h.
+//
+// QUESTÃO 2:
+// Escreva um programa que simula uma calculadora de números complexos.
+// Ele deve apresentar um menu com as operações de soma, subtração,
+// multiplicação, divisão, módulo e conjugado. Após o usuário escolher uma das
+// funções, o programa deve ler o valor dos parâmetros e em seguida exibir o
+// resultado da função.
+//
+// QUESTÃO 3:
+// Escreva uma função "void CalcularRaizes(float a, float b, float c,
+// Complexo* r1, Complexo* r2)" que recebe os coeficientes
+// de uma equação de segundo grau "a.x^2 + b.x + c = 0" e retorna por r1 e r2
+// as raízes (possivelmente complexas) desta equação.
+//
+// QUESTÃO 4:
+// Escreva um programa que lê do teclado os coeficientes reais de uma equação de
+// segundo grau e imprime na tela as raízes (possivelmente complexas) desta
+// equação.
+
+#include <cmath>
 #include <iostream>
+#include <sstream>
+#include <windows.h>
+#include "complexo.h"
 
 using namespace std;
 
-void imprimirList(const list<string> lista){
-    for (string E : lista){
-        cout << E << endl;
-    }
+double CalcularDelta(double a, double b, double c){
+  double delta = (b * b - 4*a*c);
+  return(delta);
 }
 
-int main(){
+void CalcularRaizes(double a, double b, double c, Complexo& r1, Complexo& r2){
+  double delta = (CalcularDelta(a, b, c));
+ 
+  if (delta >= 0){
+    r1 = Complexo((-b + sqrt(delta))/2*a, 0);
+    r2 = Complexo((-b - sqrt(delta))/2*a, 0);
+  } else {
+    r1 = Complexo((-b/2*a),(sqrt(-delta)/2*a));
+    r2 = Complexo((-b/2*a),(- sqrt(-delta)/2*a));
+  }
+}
 
-    cout << "Bem vindo ao leitor de Arquivos" << endl;
-
-    string nomeSaida = "Saida.txt";
-    cout << "NomeSaida: " << nomeSaida << endl;
-
-    Diretorio meuDiretorio;
-    cout << "Nome: " << meuDiretorio.lerNome() << endl
-         << "Endereco: " << meuDiretorio.lerEndereco() << endl
-         << "Nome Completo: " << meuDiretorio.lerNomeCompleto() << endl;
-
-    list<string> arquivosPLer;
-    acharArquivosPadrao(arquivosPLer);
-    imprimirList(arquivosPLer);
+void imprimeComplexo(Complexo x){
+  if (x.real() != 0 && x.imag() != 0){
+    cout << x.real() << " + " << x.imag() << "i \n";
+  } else if (x.imag() == 0){
+    cout << x.real() << endl;
+  } else if (x.real() == 0){
+    cout << x.imag() << "i \n";
+  }
+}
 
 
-    lerArquivos(meuDiretorio, arquivosPLer, nomeSaida);
+int main() {
+  double real, imaginaria, a, b, c;
+  Complexo numero, X, Y;
 
-    cout << "O arquivo Saida.txt deve ter sido preenchido." << endl;
+  int caso = 1;
+  while(caso > 0){
 
-    cout << "Testando a criacao e leitura de um indice: " << endl;
+    cout << "\tBem vindo(a) a calculadora de numeros complexos!\n" << endl
+         << "Por favor, digite um numero correspondente a uma operacao a ser realizada.\n" << endl;
 
-    
+    cout << "1 - Soma de dois numeros complexos." << endl
+         << "2 - Subtracao de dois numeros complexos." << endl
+         << "3 - Multiplicacao de dois numeros complexos." << endl
+         << "4 - Divisao de um numero complexo por outro." << endl
+         << "5 - Encontrar o Modulo de um numero complexo." << endl
+         << "6 - Encontrar o Conjugado de um numero complexo." << endl
+         << "7 - Encontrar o Inverso de um numero complexo." << endl
+         << "8 - Achar as Raizes de uma Funcao do 2o Grau." << endl
+         << "0 - Fechar o programa.\n" << endl
+         << "Entrada: ";
+        
+      
+    cin >> caso;
 
-    Indice meuIndice(meuDiretorio);
-    meuIndice.imprimirIndiceCompleto();
+    switch (caso){
 
-    cout << "Testando leitor de um arquivo:" << endl;
+      case 0:
+        cout << "Fechando o Programa..." << endl;
+        break;
 
-    list<string> palavras;
-    meuDiretorio.setNome("bruno.txt");
-    meuDiretorio.concatenarEndereco();
-    cout << "Lendo todas as palavras de [" << meuDiretorio.lerNomeCompleto() << "]" << endl;;
-    lerUmArquivo(meuDiretorio, palavras);
-    imprimirList(palavras);
+      case 1:
+        cout << "Operacao selecionada: Soma." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
 
-    string pergunta;
-    cout << "Digite uma palavra para saber quantas vezes ela apareceu em todos os documentos: " << endl;
-    cin >> pergunta;
-    cout << "A palavra ["  << pergunta << "] apareceu um total de [" 
-         << meuIndice.aparicoesTotal(pergunta) << "] vezes no seu Indice Invertido." << endl;
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo Y: " << endl;
+        cin >> real >> imaginaria;
+        Y = Complexo(real,imaginaria);
 
-    string expressaoBusca;
-    cout << "Digite uma expressao de busca: " << endl;
-    cin >> expressaoBusca;
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Numero Y: ";
+        imprimeComplexo(Y);
+        cout << "Soma X + Y: ";
+        imprimeComplexo(X + Y);
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
 
-    Busca q(expressaoBusca, meuIndice);
-    map<double, string> resposta = q.rankingCosseno(meuIndice);
-    cout << "A pesquisa retornou" << resposta.size() <<"Documentos. Documentos relacionados: " << endl;
-    map<double, string>::iterator it;
-    for (it = resposta.begin(); it != resposta.end(); it++){
-          cout << it->second << endl;
+      case 2:
+        cout << "Operacao selecionada: Subtracao." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo Y: " << endl;
+        cin >> real >> imaginaria;
+        Y = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Numero Y: ";
+        imprimeComplexo(Y);
+        cout << "Subtracao X - Y: ";
+        imprimeComplexo(X - Y);
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+
+      case 3:
+        cout << "Operacao selecionada: Multiplicacao." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo Y: " << endl;
+        cin >> real >> imaginaria;
+        Y = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Numero Y: ";
+        imprimeComplexo(Y);
+        cout << "Multiplicacao X * Y: ";
+        imprimeComplexo(X * Y);
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+
+      case 4:
+        cout << "Operacao selecionada: Divisao." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo Y: " << endl;
+        cin >> real >> imaginaria;
+        Y = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Numero Y: ";
+        imprimeComplexo(Y);
+        cout << "Divisao X / Y: ";
+        imprimeComplexo(X / Y);
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+
+      case 5:
+        cout << "Operacao selecionada: Modulo." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Modulo de X: " << X.modulo();
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+      
+      case 6:
+        cout << "Operacao selecionada: Conjugado." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Conjugado de X: ";
+        imprimeComplexo(X.conjugado());
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+
+      case 7:
+        cout << "Operacao selecionada: Inverso." << endl;
+             
+        cout << "Por favor digite a parte real e imaginaria de um numero complexo X: " << endl;
+        cin >> real >> imaginaria;
+        X = Complexo(real,imaginaria);
+
+        cout << "Numero X: ";
+        imprimeComplexo(X);
+        cout << "Inverso de X: ";
+        imprimeComplexo(X.inverso());
+        cout << "\n\n\n";
+        system("pause");
+        system("cls");
+        break;
+      
+      case 8:
+        cout << "Operacao selecionada: Raizes de uma Funcao do 2o Grau." << endl
+             << "Funcao: aX2 + bX + c" << endl;
+
+        cout << "Digite a: ";
+        cin >> a;
+        cout << "Digite b: ";
+        cin >> b;
+        cout << "Digite c: ";
+        cin >> c;
+
+        cout << "Funcao montada: " << a << "x`2 + " << b << "x + "<< c << endl;
+
+        CalcularRaizes(a, b, c, X, Y);
+        cout << "Raiz 1: ";
+        imprimeComplexo(X);       
+        cout << "Raiz 2: ";
+        imprimeComplexo(Y);
+        
+        system("pause");
+        system("cls");
+        break;
+      
+      default:
+        cout << "Numero de entrada invalido, reiniciando...\n" << endl;
+        system("pause");
+        system("cls");
+        break;
     }
-    cout << "FIM DA PESQUISA" << endl;    
+  }
 
     system("pause");
     return 0;
